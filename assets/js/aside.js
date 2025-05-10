d7d189317d9d646e48b7442649e22fa81a40f526
AnnaStudentKDGIOT <147697482+AnnaStudentKDGIOT@users.noreply.github.com>
2025-05-10 18:28:33 +0200

    aside open and close buttons

--- assets/js/aside.js
+++ assets/js/aside.js
@@ -2,4 +2,8 @@
   document.getElementById('toggleSidebar').addEventListener('click', function () {
     document.getElementById('mySidebar').classList.toggle('active');
   });
+  document.getElementById('closeAside').addEventListener('click', () => {
+    document.getElementById('myAside').classList.add('closed');
+  });
+  
 
