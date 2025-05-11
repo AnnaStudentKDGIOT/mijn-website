3ba99eeae16d91c59bafbf68c6a5c6b165401549
AnnaStudentKDGIOT <147697482+AnnaStudentKDGIOT@users.noreply.github.com>
2025-05-11 16:42:35 +0200

    sidebar button js adjustment button toggler JS CLEANING

--- assets/js/aside.js
+++ assets/js/aside.js
@@ -2,8 +2,6 @@
   document.getElementById('toggleSidebar').addEventListener('click', function () {
     document.getElementById('mySidebar').classList.toggle('active');
   });
-  document.getElementById('closeAside').addEventListener('click', () => {
-    document.getElementById('myAside').classList.add('closed');
-  });
+ 
   
 
