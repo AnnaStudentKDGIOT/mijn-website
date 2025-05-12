93af3b37f92d8ea439fda81f6da24d501414de8c
AnnaStudentKDGIOT <147697482+AnnaStudentKDGIOT@users.noreply.github.com>
2025-05-12 19:01:56 +0200

    aside cleaning up link texts

--- assets/js/aside.js
+++
@@ -1,8 +0,0 @@
-const aside = document.getElementById('mySidebar');
-const toggleBtn = document.getElementById('toggleSidebar');
-
-// Toggle aside visibility
-toggleBtn.addEventListener('click', () => {
-  aside.classList.toggle('open');
-  aside.classList.toggle('closed');
-});
