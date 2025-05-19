96c9e427ffdc9e7ac0dbce6a745de8ce5f932f1e
AnnaStudentKDGIOT <147697482+AnnaStudentKDGIOT@users.noreply.github.com>
2025-05-19 20:10:03 +0200

    leaflet cooordinaten

--- assets/js/leaflet.js
+++ assets/js/leaflet.js
@@ -1,6 +1,6 @@
 
 
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([51.260197, 4.402771], 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
