6c89d48f5999ae445a4b07da4eaffb4d226570d4
AnnaStudentKDGIOT <147697482+AnnaStudentKDGIOT@users.noreply.github.com>
2025-09-07 02:24:16 +0200

    leaflet map

--- assets/js/leaflet.js
+++ assets/js/leaflet.js
@@ -1,7 +1,14 @@
 
+<div id="map" style="height: 400px;"></div>
+    // Initialize the map just above Antwerp center
+    var map = L.map('map').setView([51.225, 4.4025], 13);
 
-var map = L.map('map').setView([51.260197, 4.402771], 13);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+    // Tile layer
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+    // Marker at Antwerp city center
+    var marker = L.marker([51.2194, 4.4025]).addTo(map);
+    marker.bindPopup("<b>Antwerp Center</b>").openPopup();
